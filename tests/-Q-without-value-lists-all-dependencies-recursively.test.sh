ae7b0d6312c0ed9c334300e5b8bde5b47c3a7c91
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-06-15 01:32:29 +0600

    tests: add testcase for --recursive flag

---
+++ tests/-Q-without-value-lists-all-dependencies-recursively.test.sh
@@ -0,0 +1,31 @@
+:project "dir1/main.go" <<GO
+package main
+
+import "github.com/kovetskiy/manul-test-foo"
+
+func main() {
+    foo.A()
+}
+GO
+
+:project "dir2/main.go" <<GO
+package main
+
+import "github.com/kovetskiy/manul-test-bar"
+import "github.com/kovetskiy/manul-test-foo"
+
+func main() {
+    foo.A()
+    bar.A()
+}
+GO
+
+:lib "github.com/kovetskiy/manul-test-foo"
+:lib "github.com/kovetskiy/manul-test-bar"
+
+
+tests:ensure :manul -Q --recursive \| sort -n
+tests:assert-no-diff stdout <<VENDORS
+github.com/kovetskiy/manul-test-foo
+github.com/kovetskiy/manul-test-bar
+VENDORS
